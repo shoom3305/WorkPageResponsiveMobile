780905f73e2a28e64536cfb4c6804e6389d85752
Hairless <53937947+shoom3305@users.noreply.github.com>
2020-10-12 15:39:03 +0600

    Add files via upload

---
+++ shoom.rework/main.js
@@ -0,0 +1,48 @@
+﻿let hamburger = document.querySelector('.hamburger');
+let menu = document.querySelector('.closed_menu');
+let left = document.querySelector('.left');
+let scroll = document.querySelector('.scroll') ;
+
+
+let float = document.querySelector('.float');
+let floating = document.querySelector('.floating');
+let flik = document.querySelector('.flip');
+let flop = document.querySelector('.flok');
+
+
+///Hamburger///
+hamburger.addEventListener('click', function(){
+
+    menu.classList.toggle('active');
+    hamburger.classList.toggle('right') ;
+});
+//to scroll//
+
+
+float.addEventListener('click', function(){
+
+    this.classList.add('passive');
+    floating.classList.remove('passive');
+    flik.classList.remove('passive');
+    flop.classList.remove('passive');
+})  ;
+floating.addEventListener('click', function(){
+    this.classList.add('passive');
+    float.classList.remove('passive');
+    flik.classList.remove('passive');
+    flop.classList.remove('passive');
+})   ;
+flik.addEventListener('click',function(){
+    this.classList.add('passive');
+    float.classList.remove('passive');
+    floating.classList.remove('passive');
+    flop.classList.remove('passive');
+})  ;
+flop.addEventListener('click',function(){
+    this.classList.add('passive');
+    float.classList.remove('passive');
+    floating.classList.remove('passive');
+    flik.classList.remove('passive');
+});
+
+
